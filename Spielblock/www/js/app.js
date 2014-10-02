0a6f39a5b9b886a3907c577edd71f91354456cec
Dr4K4n <stefan@dr4k4n.de>
2014-10-02 16:24:19 +0200

    implemented basic functionality

--- Spielblock/www/js/app.js
+++ Spielblock/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngStorage'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -44,7 +44,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       views: {
         'menuContent': {
           templateUrl: 'templates/pad.html',
-          controller: 'PadCtrl as ctrl'
+          controller: 'PadCtrl'
         }
       }
     })
--- Spielblock/www/js/controllers.js
+++ Spielblock/www/js/controllers.js
@@ -1,8 +1,49 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope) {
+.controller('AppCtrl', function($scope, $localStorage) {
+  $scope.reset = function() {
+    $localStorage.$reset();
+    $localStorage.players = [];
+    $localStorage.rounds = [{
+      number: 1,
+      scores: []
+    }];
+  };
 })
 
-.controller('PadCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+.controller('PadCtrl', function($scope, $localStorage) {
+  $scope.data = $localStorage;
+
+  // Initialize if necessary
+  if(angular.isUndefined($scope.data.players)) {
+    $scope.data.players = [];
+  }
+
+  // Initialize if necessary
+  if(angular.isUndefined($scope.data.rounds)) {
+    $scope.data.rounds = [{
+      number: 1,
+      scores: []
+    }];
+  }
+  
+  // add player to array
+  $scope.addPlayer = function() {
+    $scope.data.players.push({name:''});
+    angular.forEach($scope.data.rounds, function(round, key) {
+      round.scores.push({score: 0});
+    });
+  };
+
+  // add round
+  $scope.addRound = function() {
+    var round = {
+      number: $scope.data.rounds.length +1,
+      scores: []
+    };
+    angular.forEach($scope.data.players, function(player) {
+      round.scores.push({score: 0});
+    });
+    $scope.data.rounds.push(round);
+  }
+});
