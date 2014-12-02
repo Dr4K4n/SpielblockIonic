7749173347ec845ea78164ecae1707f73e07b7eb
Dr4K4n <stefan@dr4k4n.de>
2014-12-02 23:42:41 +0100

    added default Player names, did some code formatting (HTML-CSS-JS Prettify)
    closes #3

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,6 @@
 // Ionic Starter App
 
+/* globals cordova, StatusBar */
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
@@ -7,38 +8,38 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'ngStorage'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+.run(function ($ionicPlatform) {
+  'use strict';
+
+  $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
+  'use strict';
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-
-    // setup an abstract state for the menus directive
+  // setup an abstract state for the menus directive
     .state('app', {
-      url: "/app",
+      url: '/app',
       abstract: true,
-      templateUrl: "templates/menu.html",
+      templateUrl: 'templates/menu.html',
       controller: 'AppCtrl'
     })
-
     // Each menu has its own nav history stack:
-
     .state('app.pad', {
       url: '/pad',
       views: {
@@ -47,10 +48,9 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngStorage'])
           controller: 'PadCtrl'
         }
       }
-    })
+    });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/pad');
 
 });
-
